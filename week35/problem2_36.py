8fd5d232446f4027d4ee7e4755ff25766198ac4b
salomaestro <chris10an.salomonsen@gmail.com>
2021-09-10 20:21:00 +0200

    fix

--- week35/problem2_36.py
+++ week35/problem2_36.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
 
 np.random.seed(2)
-
 
 def unknown_pdf(x):
     if x > 0 and x < 2:
@@ -11,34 +11,30 @@ def unknown_pdf(x):
         return 0
 
 
-x_vals = np.arange(-1, 3, .1)
-x_data = np.array(list(map(unknown_pdf, x_vals)))
+x = np.random.uniform(0, 2, size=(5000,1))
+y = np.array(list(map(unknown_pdf, x)))
 
 
 class ParzenWindow:
-    def __init__(self, x_i, b):
-        self.x_i = x_i
+    def __init__(self, x, b):
+        self.x = x
         self.b = b
-        self.N = len(self.x_i)
-
-    def prob(self, x):
-        pulsenum = self.gaussian_pulse((self.x_i - x) / self.b)
-        print(pulsenum[0])
-        return 1 / (self.b * self.N) * np.sum(pulsenum, axis=1)
-
-    @staticmethod
-    def pulse(x):
-        return np.where(np.abs(x) <= .5, 1, 0)
+        self.N = len(self.x)
+ 
+    def prob(self):
+        dist = squareform(pdist(self.x))
+        pulsenum = self.gaussian_pulse(dist / (self.b ** 2))
+        
+        return 1 / self.N * np.sum(pulsenum, axis=1)
 
     def gaussian_pulse(self, x):
-        return 1 / np.sqrt(2 * np.pi) * np.exp(- 0.5 * np.dot(x.T, x))
+        return 1 / (np.sqrt(2 * np.pi) * self.b) * np.exp(- 0.5 * x)
 
 
-c1 = ParzenWindow(x_data, 1)
-x = np.random.uniform(-5, 6, size=(3, 1))
+c1 = ParzenWindow(x, .2)
 
-b1 = c1.prob(x)
-# print(b1)
-# plt.plot(x_vals, b1)
-# plt.plot(x_vals, x_data)
-# plt.show()
+b1 = c1.prob()
+
+plt.scatter(x, b1)
+plt.plot(x, y)
+plt.show()
