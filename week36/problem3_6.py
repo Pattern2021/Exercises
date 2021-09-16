fd7ac1d84afd8fcd832c477d804424e0c7cc1862
Christian Salomonsen <55956280+salomaestro@users.noreply.github.com>
2021-09-16 15:40:54 +0200

    Seems to work fine

--- week36/problem3_6.py
+++ week36/problem3_6.py
@@ -4,7 +4,7 @@ from time import time
 
 from classifiers.Linear import LinearClassifier
 
-class LMS(LinearClassifier):
+class LeastMeanSquares(LinearClassifier):
     """
     Linear Clasifier using Least means squares criterion. Inherits from Linear Classifier base class.
 
@@ -16,6 +16,8 @@ class LMS(LinearClassifier):
     """
     def __init__(self, Xtr1, Xtr2):
         super().__init__(Xtr1, Xtr2)
+
+        self.name = "Least Mean Squares"
 
     def train(self, rho=0.01, threshold=1e-5):
         self.trained = True
@@ -39,32 +41,8 @@ class LMS(LinearClassifier):
 
                 self.w = self.w + rho_ * x @ (y - x.T @ self.w)
             it += 1
-        print("Training runs over dataset: ", it)
-
-        self.w = self.w.flatten()
-        w2 = self.w[0]
-        w1 = self.w[1]
-        w0 = self.w[2]
-        slope = (-w0 / w2) / (w0 / w1)
-        intercept = -w0 / w2
-        xarr = np.arange(np.min(self.Xtr[:, 0]), np.max(self.Xtr[:, 0]), 0.1)
-        plt.plot(xarr, slope * xarr + intercept)
-        plt.scatter(self.Xtr1[:, 0], self.Xtr1[:, 1])
-        plt.scatter(self.Xtr2[:, 0], self.Xtr2[:, 1])
-        plt.show()
-
-    def test(self):
-        self.tested = True
-        pass
 
 def main():
-    # np.random.seed(1)
-    seed = np.random.randint(0, 1e8)
-    # weird situation. seed = 88857357 yields a case where algorithm does not work, but seed = 49361358 does work well
-    print("Seed: ", seed)
-    np.random.seed(seed)
-
-
     mu1 = np.array([1, 1])
     mu2 = np.array([3, 3])
     sigma1 = .2
@@ -73,8 +51,15 @@ def main():
     data1 = np.random.normal(mu1, sigma1, size=(100, 2))
     data2 = np.random.normal(mu2, sigma2, size=(100, 2))
 
-    ins = LMS(data1, data2)
+    ins = LeastMeanSquares(data1, data2)
     ins.train()
+    ins.plot_training()
+
+    test1 = np.random.normal(mu1, sigma1, size=(30, 2))
+    test2 = np.random.normal(mu2, sigma2, size=(30, 2))
+
+    ins.test(test1, test2)
+    ins.plot_testing()
 
 if __name__ == '__main__':
     main()
