75a62ae422e1eea4d3f227fa00b8578b54ba0b4c
Christian Salomonsen <55956280+salomaestro@users.noreply.github.com>
2021-10-03 17:54:03 +0200

    had made it a module but makes more sense as script

--- week38/problem4_2.py
+++ week38/problem4_2.py
@@ -3,57 +3,52 @@ import matplotlib.pyplot as plt
 from networks.multilayer_perceptron import Multilayer_perceptron
 
 
-def main():
-    # np.random.seed(6969)
+# np.random.seed(6969)
 
-    mu1 = np.array([0, 0])
-    mu2 = np.array([1, 1])
-    mu3 = np.array([0, 1])
-    mu4 = np.array([1, 0])
+mu1 = np.array([0, 0])
+mu2 = np.array([1, 1])
+mu3 = np.array([0, 1])
+mu4 = np.array([1, 0])
 
-    covariance = np.diag((0.01, 0.01))
+covariance = np.diag((0.01, 0.01))
 
-    N = 100
-    cls1_vec1 = np.random.normal(mu1, np.diag(covariance), size=(N, 2))
-    cls1_vec2 = np.random.normal(mu2, np.diag(covariance), size=(N, 2))
-    cls2_vec1 = np.random.normal(mu3, np.diag(covariance), size=(N, 2))
-    cls2_vec2 = np.random.normal(mu4, np.diag(covariance), size=(N, 2))
+N = 100
+cls1_vec1 = np.random.normal(mu1, np.diag(covariance), size=(N, 2))
+cls1_vec2 = np.random.normal(mu2, np.diag(covariance), size=(N, 2))
+cls2_vec1 = np.random.normal(mu3, np.diag(covariance), size=(N, 2))
+cls2_vec2 = np.random.normal(mu4, np.diag(covariance), size=(N, 2))
 
-    # cls1_vec1_x, cls1_vec1_y = np.random.multivariate_normal(mu1, covariance, size=(N, 2))
-    # cls1_vec2_x, cls1_vec2_y = np.random.multivariate_normal(mu2, covariance, size=(N, 2))
-    # cls2_vec1_x, cls2_vec1_y = np.random.multivariate_normal(mu3, covariance, size=(N, 2))
-    # cls2_vec2_x, cls2_vec2_y = np.random.multivariate_normal(mu4, covariance, size=(N, 2))
+# cls1_vec1_x, cls1_vec1_y = np.random.multivariate_normal(mu1, covariance, size=(N, 2))
+# cls1_vec2_x, cls1_vec2_y = np.random.multivariate_normal(mu2, covariance, size=(N, 2))
+# cls2_vec1_x, cls2_vec1_y = np.random.multivariate_normal(mu3, covariance, size=(N, 2))
+# cls2_vec2_x, cls2_vec2_y = np.random.multivariate_normal(mu4, covariance, size=(N, 2))
 
-    # cls1_vec1 = np.c_[cls1_vec1_x, cls1_vec1_y]
-    # cls1_vec2 = np.c_[cls1_vec2_x, cls1_vec2_y]
-    # cls2_vec1 = np.c_[cls2_vec1_x, cls1_vec2_y]
-    # cls2_vec2 = np.c_[cls2_vec2_x, cls2_vec2_y]
+# cls1_vec1 = np.c_[cls1_vec1_x, cls1_vec1_y]
+# cls1_vec2 = np.c_[cls1_vec2_x, cls1_vec2_y]
+# cls2_vec1 = np.c_[cls2_vec1_x, cls1_vec2_y]
+# cls2_vec2 = np.c_[cls2_vec2_x, cls2_vec2_y]
 
-    # print(cls1_vec1)
-    # exit()
+# print(cls1_vec1)
+# exit()
 
-    cls1 = np.concatenate((cls1_vec1, cls1_vec2), axis=0)
-    cls2 = np.concatenate((cls2_vec1, cls2_vec2), axis=0)
-    y1 = np.ones(len(cls1[:, 0]))
-    y2 = np.zeros(len(cls2[:, 0]))
+cls1 = np.concatenate((cls1_vec1, cls1_vec2), axis=0)
+cls2 = np.concatenate((cls2_vec1, cls2_vec2), axis=0)
+y1 = np.ones(len(cls1[:, 0]))
+y2 = np.zeros(len(cls2[:, 0]))
 
 
-    Xtraining = np.concatenate((cls1, cls2), axis=0)
-    Ytraining = np.atleast_2d(np.concatenate((y1, y2), axis=0))
-    hidden_neurons = 2
-    network_shape = np.array([2, hidden_neurons, 1])
+Xtraining = np.concatenate((cls1, cls2), axis=0)
+Ytraining = np.atleast_2d(np.concatenate((y1, y2), axis=0))
+hidden_neurons = 2
+network_shape = np.array([2, hidden_neurons, 1])
 
-    learning_rates = np.logspace(-1.5, -2, 6)
-    fig, axs = plt.subplots(2, len(learning_rates), figsize=(16, 8), sharey="row")
-    
-    for i, lr in enumerate(learning_rates):
-        ins = Multilayer_perceptron(Xtraining, Ytraining, network_shape)
-        ins.train(lr, epochs=1000, alpha=0, ax=axs[0, i])
-        ins.plot_training(axs[1, i], lr)
-    fig.suptitle("2-class neural network with one hidden layer of {} neurons".format(hidden_neurons))
-    fig.tight_layout()
-    plt.show()
-    
+learning_rates = np.logspace(-1.5, -2, 6)
+fig, axs = plt.subplots(2, len(learning_rates), figsize=(16, 8), sharey="row")
 
-if __name__ == "__main__":
-    main()
+for i, lr in enumerate(learning_rates):
+    ins = Multilayer_perceptron(Xtraining, Ytraining, network_shape)
+    ins.train(lr, epochs=1000, alpha=0, ax=axs[0, i])
+    ins.plot_training(axs[1, i], lr)
+fig.suptitle("2-class neural network with one hidden layer of {} neurons".format(hidden_neurons))
+fig.tight_layout()
+plt.show()
